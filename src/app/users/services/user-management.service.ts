6701056d08dcbf2fab2360e63da5874049b0a093
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-12-20 14:55:03 -0300

    feat: finished user-management service and add IqueryParams.

--- src/app/users/services/user-management.service.ts
+++ src/app/users/services/user-management.service.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { IGetUsers } from '../interfaces/igetUsers';
+import { IQueryParams } from '../interfaces/IQueryParams';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +12,41 @@ export class UserManagementService {
   private http = inject(HttpClient);
   public errors: string[] = [];
 
-  async getUsers(){
-    
+  async getUsers(IQueryParams: IQueryParams): Promise<IGetUsers[]>
+  {
+    try
+    {
+      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhcmxvczUxMzJmY0BnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiY2FybG9zNTEzMmZjQGdtYWlsLmNvbSIsIm5hbWVpZCI6ImMyMDVkY2QxLTE5NmEtNGNiMi1hMjZhLTBmMDFmZTM5OWFhYyIsImp0aSI6Ijc4NzBmNWFjLTRlYzMtNGVlNy04NmExLTc5M2JhN2ZiYjQ0MyIsInJvbGUiOiJVc2VyIiwibmJmIjoxNzM0NzExODgzLCJleHAiOjE3MzQ3OTgyODMsImlhdCI6MTczNDcxMTg4MywiaXNzIjoiaGh0cHM6Ly9sb2NhbGhvc3Q6NTAwMCIsImF1ZCI6ImhodHBzOi8vbG9jYWxob3N0OjUwMCJ9.lwwNdptdI6X1sgBRNqjvr2SDK3TsVxWAaxB3v45Daw_tqzaLfg4C-h-qvOhSvMSNgwGIO3IyC8ZmmZexIRFG9g';
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+      let params = new HttpParams()
+        if(IQueryParams.name) params = params.set('name', IQueryParams.name);
+        if(IQueryParams.page) params = params.set('page', IQueryParams.page.toString());
+        if(IQueryParams.pageSize) params = params.set('pageSize', IQueryParams.pageSize.toString());
+
+      const response = await firstValueFrom(this.http.get<IGetUsers[]>(`${this.baseUrl}/GetUsers`, {params: params, headers: headers}))
+      return Promise.resolve(response);
+    } catch (error) {
+      console.log("Error in getUsers service", error);
+      let e = error as HttpErrorResponse;
+      this.errors.push(e.message);
+      return Promise.reject(e);
+    }
+
   }
 
-  async changeState(){
-
+  async changeState(email: string): Promise<IGetUsers>
+  {
+    try
+    {
+      const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhcmxvczUxMzJmY0BnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiY2FybG9zNTEzMmZjQGdtYWlsLmNvbSIsIm5hbWVpZCI6ImMyMDVkY2QxLTE5NmEtNGNiMi1hMjZhLTBmMDFmZTM5OWFhYyIsImp0aSI6Ijc4NzBmNWFjLTRlYzMtNGVlNy04NmExLTc5M2JhN2ZiYjQ0MyIsInJvbGUiOiJVc2VyIiwibmJmIjoxNzM0NzExODgzLCJleHAiOjE3MzQ3OTgyODMsImlhdCI6MTczNDcxMTg4MywiaXNzIjoiaGh0cHM6Ly9sb2NhbGhvc3Q6NTAwMCIsImF1ZCI6ImhodHBzOi8vbG9jYWxob3N0OjUwMCJ9.lwwNdptdI6X1sgBRNqjvr2SDK3TsVxWAaxB3v45Daw_tqzaLfg4C-h-qvOhSvMSNgwGIO3IyC8ZmmZexIRFG9g';
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+      const response = await firstValueFrom(this.http.post<IGetUsers>(`${this.baseUrl}/ChangeStateUser/${email}`, { headers: headers }))
+      return Promise.resolve(response);
+    } catch (error){
+      console.log("Error in changeState service", error);
+      let e = error as HttpErrorResponse;
+      this.errors.push(e.message);
+      return Promise.reject(e)
+    }
   }
 }
