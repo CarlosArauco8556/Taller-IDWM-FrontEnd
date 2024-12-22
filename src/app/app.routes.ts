ae20bff016541daf9a09eb54dd2b1e7cdbe9338e
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-12-22 16:05:51 -0300

    feat: the sidebar is added to be able to filter by product type as a component of the home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        pathMatch: 'full',
+        loadComponent: () => import('./home/pages/home-page/home-page.component').then(m => m.HomePageComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
