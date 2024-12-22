a48d9a5df013870712aa1c77c1821231936cebc7
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-12-22 16:39:34 -0300

    feat: the component is added to be able to view the products

--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from "../../components/side-bar/side-bar.component";
 import { RouterOutlet } from '@angular/router';
+import { ProductCardComponent } from "../../components/product-card/product-card.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [SideBarComponent, RouterOutlet],
+  imports: [SideBarComponent, ProductCardComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
