2bcf2e734479ef482d12b50cefe331b9827c11ba
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-12-23 11:23:55 -0300

    fix: structure classes.

--- src/app/users/services/account.service.ts
+++ src/app/users/services/account.service.ts
@@ -13,31 +13,28 @@ export class AccountService {
   private http = inject(HttpClient);
   public errors: string[] = [];
 
-  async editProfile(IEditProfile: IEditProfile): Promise<string>
-  {
-    try{
+  async editProfile(IEditProfile: IEditProfile): Promise<string> {
+    try {
       const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhcmxvczUxMzJmY0BnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiY2FybG9zNTEzMmZjQGdtYWlsLmNvbSIsIm5hbWVpZCI6ImMyMDVkY2QxLTE5NmEtNGNiMi1hMjZhLTBmMDFmZTM5OWFhYyIsImp0aSI6Ijc4NzBmNWFjLTRlYzMtNGVlNy04NmExLTc5M2JhN2ZiYjQ0MyIsInJvbGUiOiJVc2VyIiwibmJmIjoxNzM0NzExODgzLCJleHAiOjE3MzQ3OTgyODMsImlhdCI6MTczNDcxMTg4MywiaXNzIjoiaGh0cHM6Ly9sb2NhbGhvc3Q6NTAwMCIsImF1ZCI6ImhodHBzOi8vbG9jYWxob3N0OjUwMCJ9.lwwNdptdI6X1sgBRNqjvr2SDK3TsVxWAaxB3v45Daw_tqzaLfg4C-h-qvOhSvMSNgwGIO3IyC8ZmmZexIRFG9g';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-      const response = await firstValueFrom(this.http.post<{message: string}>(`${this.baseUrl}/edit-profile`, IEditProfile, {headers: headers} ))
+      const response = await firstValueFrom(this.http.post<{ message: string }>(`${this.baseUrl}/edit-profile`, IEditProfile, { headers: headers }))
       return Promise.resolve(response.message);
     } catch (error) {
-      console.log("Error in editProsile service" ,error);
+      console.log("Error in editProsile service", error);
       let e = error as HttpErrorResponse;
       this.errors.push(e.message);
       return Promise.reject(e);
     }
   }
 
-  async changePassword(IChangePassword: IChangePassword): Promise<string>
-  {
-    try
-    {
+  async changePassword(IChangePassword: IChangePassword): Promise<string> {
+    try {
       const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhcmxvczUxMzJmY0BnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiY2FybG9zNTEzMmZjQGdtYWlsLmNvbSIsIm5hbWVpZCI6ImMyMDVkY2QxLTE5NmEtNGNiMi1hMjZhLTBmMDFmZTM5OWFhYyIsImp0aSI6Ijc4NzBmNWFjLTRlYzMtNGVlNy04NmExLTc5M2JhN2ZiYjQ0MyIsInJvbGUiOiJVc2VyIiwibmJmIjoxNzM0NzExODgzLCJleHAiOjE3MzQ3OTgyODMsImlhdCI6MTczNDcxMTg4MywiaXNzIjoiaGh0cHM6Ly9sb2NhbGhvc3Q6NTAwMCIsImF1ZCI6ImhodHBzOi8vbG9jYWxob3N0OjUwMCJ9.lwwNdptdI6X1sgBRNqjvr2SDK3TsVxWAaxB3v45Daw_tqzaLfg4C-h-qvOhSvMSNgwGIO3IyC8ZmmZexIRFG9g';
       const headers = new HttpHeaders().set('Authorization', `Beare, ${token}`);
-      const response = await firstValueFrom(this.http.post<{message: string}>(`${this.baseUrl}/change-password`, IChangePassword, {headers: headers}))
+      const response = await firstValueFrom(this.http.post<{ message: string }>(`${this.baseUrl}/change-password`, IChangePassword, { headers: headers }))
       return Promise.resolve(response.message);
     } catch (error) {
-      console.log("Error in changePassword service" ,error);
+      console.log("Error in changePassword service", error);
       let e = error as HttpErrorResponse;
       this.errors.push(e.message);
       return Promise.reject(e);
--- src/app/users/services/user-management.service.ts
+++ src/app/users/services/user-management.service.ts
@@ -12,18 +12,16 @@ export class UserManagementService {
   private http = inject(HttpClient);
   public errors: string[] = [];
 
-  async getUsers(IQueryParams: IQueryParams): Promise<IGetUsers[]>
-  {
-    try
-    {
+  async getUsers(IQueryParams: IQueryParams): Promise<IGetUsers[]> {
+    try {
       const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhcmxvczUxMzJmY0BnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiY2FybG9zNTEzMmZjQGdtYWlsLmNvbSIsIm5hbWVpZCI6ImMyMDVkY2QxLTE5NmEtNGNiMi1hMjZhLTBmMDFmZTM5OWFhYyIsImp0aSI6Ijc4NzBmNWFjLTRlYzMtNGVlNy04NmExLTc5M2JhN2ZiYjQ0MyIsInJvbGUiOiJVc2VyIiwibmJmIjoxNzM0NzExODgzLCJleHAiOjE3MzQ3OTgyODMsImlhdCI6MTczNDcxMTg4MywiaXNzIjoiaGh0cHM6Ly9sb2NhbGhvc3Q6NTAwMCIsImF1ZCI6ImhodHBzOi8vbG9jYWxob3N0OjUwMCJ9.lwwNdptdI6X1sgBRNqjvr2SDK3TsVxWAaxB3v45Daw_tqzaLfg4C-h-qvOhSvMSNgwGIO3IyC8ZmmZexIRFG9g';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
       let params = new HttpParams()
-        if(IQueryParams.name) params = params.set('name', IQueryParams.name);
-        if(IQueryParams.page) params = params.set('page', IQueryParams.page.toString());
-        if(IQueryParams.pageSize) params = params.set('pageSize', IQueryParams.pageSize.toString());
+      if (IQueryParams.name) params = params.set('name', IQueryParams.name);
+      if (IQueryParams.page) params = params.set('page', IQueryParams.page.toString());
+      if (IQueryParams.pageSize) params = params.set('pageSize', IQueryParams.pageSize.toString());
 
-      const response = await firstValueFrom(this.http.get<IGetUsers[]>(`${this.baseUrl}/GetUsers`, {params: params, headers: headers}))
+      const response = await firstValueFrom(this.http.get<IGetUsers[]>(`${this.baseUrl}/GetUsers`, { params: params, headers: headers }))
       return Promise.resolve(response);
     } catch (error) {
       console.log("Error in getUsers service", error);
@@ -34,15 +32,13 @@ export class UserManagementService {
 
   }
 
-  async changeState(email: string): Promise<IGetUsers>
-  {
-    try
-    {
+  async changeState(email: string): Promise<IGetUsers> {
+    try {
       const token = localStorage.getItem('token') || 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhcmxvczUxMzJmY0BnbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiY2FybG9zNTEzMmZjQGdtYWlsLmNvbSIsIm5hbWVpZCI6ImMyMDVkY2QxLTE5NmEtNGNiMi1hMjZhLTBmMDFmZTM5OWFhYyIsImp0aSI6Ijc4NzBmNWFjLTRlYzMtNGVlNy04NmExLTc5M2JhN2ZiYjQ0MyIsInJvbGUiOiJVc2VyIiwibmJmIjoxNzM0NzExODgzLCJleHAiOjE3MzQ3OTgyODMsImlhdCI6MTczNDcxMTg4MywiaXNzIjoiaGh0cHM6Ly9sb2NhbGhvc3Q6NTAwMCIsImF1ZCI6ImhodHBzOi8vbG9jYWxob3N0OjUwMCJ9.lwwNdptdI6X1sgBRNqjvr2SDK3TsVxWAaxB3v45Daw_tqzaLfg4C-h-qvOhSvMSNgwGIO3IyC8ZmmZexIRFG9g';
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
       const response = await firstValueFrom(this.http.post<IGetUsers>(`${this.baseUrl}/ChangeStateUser/${email}`, { headers: headers }))
       return Promise.resolve(response);
-    } catch (error){
+    } catch (error) {
       console.log("Error in changeState service", error);
       let e = error as HttpErrorResponse;
       this.errors.push(e.message);
