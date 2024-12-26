89bfbfb5f6d2c68de90b35919e4dfb27f344cba0
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-12-26 20:08:42 -0300

    feat: add toggle button in nav-bar.

--- src/app/_shared/components/nav-bar/nav-bar.component.ts
+++ src/app/_shared/components/nav-bar/nav-bar.component.ts
@@ -1,22 +1,29 @@
 import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { QueryServiceService } from '../../../home/services/query-service.service';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-
   public textFilterValue: string = '';
-
   private queryService: QueryServiceService = inject(QueryServiceService);
+  menuOpen = false;
+
+  constructor(private router: Router) {}
 
   searchProducts(): void {
+    this.router.navigate(['/home']);
     this.queryService.updateFilters({ textFilter: this.textFilterValue }); 
   }
 
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
 }
