9e64e02946f63c6aaf3f714a12d3939f2966d555
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-12-26 15:56:31 -0300

    feat: added a message when adding a product to shopping cart

--- src/app/home/components/product-card/product-card.component.ts
+++ src/app/home/components/product-card/product-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../interfaces/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
@@ -13,6 +14,8 @@ export class ProductCardComponent {
   @Input() product: Product;
 
   @Output() productForCart = new EventEmitter<Product>();
+
+  showMessage = false;
 
   constructor() { 
     this.product = {
@@ -30,6 +33,10 @@ export class ProductCardComponent {
 
   addProductToCart(): void {
     this.productForCart.emit(this.product);
+    this.showMessage = true;
+    setTimeout(() => {
+      this.showMessage = false;
+    }, 3000);
   }
 
 }
