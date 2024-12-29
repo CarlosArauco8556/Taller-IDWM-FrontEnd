329ff902180916bc634f4a7b09ec0fc76d828130
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-12-28 22:14:07 -0300

    feat: options are added to register new users

---
+++ src/app/auth/interfaces/SignUp.ts
@@ -0,0 +1,9 @@
+export interface SignUp {
+    rut:             string;
+    name:            string;
+    dateOfBirth:     Date;
+    gender:          string;
+    email:           string;
+    password:        string;
+    confirmPassword: string;
+}
--- src/app/auth/services/auth-service.service.ts
+++ src/app/auth/services/auth-service.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { LogIn } from '../interfaces/LogIn';
 import { AuthInfo } from '../interfaces/AuthInfo';
 import { firstValueFrom } from 'rxjs';
+import { SignUp } from '../interfaces/SignUp';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,17 @@ export class AuthServiceService {
       return Promise.resolve(response);
     } catch (error) {
       console.log("Error in login service", error);
+      this.errors.push((error as any).message);
+      return Promise.reject(error);
+    }
+  }
+
+  async signUp(signUp: SignUp): Promise<AuthInfo> {
+    try {
+      const response = await firstValueFrom(this.httpclient.post<AuthInfo>(`${this.baseUrl}/register`, signUp));
+      return Promise.resolve(response);
+    } catch (error) {
+      console.log("Error in signUp service", error);
       this.errors.push((error as any).message);
       return Promise.reject(error);
     }
